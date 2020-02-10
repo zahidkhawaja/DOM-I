0e4e15879f95da5c3b40b1a0b0f068697ba9ecca
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-02-10 17:40:24 -0500

    Add innerHTML and complete contact section

--- js/index.js
+++ js/index.js
@@ -42,17 +42,23 @@ let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"]);
 
 let navLinks = document.getElementsByTagName("a");
+let navArray = Array.from(navLinks);
 
-Array.from(navLinks).forEach(link => {
+for(i=0; i < navArray.length; i++) {
+let navText = navLinks[i];
+navText.textContent = siteContent["nav"][`nav-item-${i+1}`];
+}
+
+navArray.forEach(link => {
   link.style.color = "green";
-  link.textContent = "About";
-})
+});
 
 let headerImg = document.getElementById("cta-img");
 headerImg.setAttribute("src", siteContent["cta"]["img-src"]);
 
 let headerText = document.querySelector("h1");
 headerText.textContent = siteContent["cta"]["h1"];
+headerText.innerHTML = "DOM<br> Is<br> Awesome";
 
 let buttonText = document.querySelector("button");
 buttonText.textContent = siteContent["cta"]["button"];
@@ -75,11 +81,15 @@ servicesP.textContent = siteContent["main-content"]["services-content"];
 let contactTitle = document.querySelector(".contact h4");
 contactTitle.textContent = siteContent["contact"]["contact-h4"];
 
-let contactP = document.querySelectorAll(".contact p");
+let addressP = document.querySelector(".contact p");
+addressP.textContent = siteContent["contact"]["address"];
+addressP.innerHTML = "123 Way 456 Street<br>Somewhere, USA";
 
-Array.from(contactP).forEach(p => {
-  p.textContent = "Paragraph";
-});
+let numberP = document.querySelector(".contact p:nth-of-type(2)");
+numberP.textContent = siteContent["contact"]["phone"];
+
+let emailP = document.querySelector(".contact p:last-child");
+emailP.textContent = siteContent["contact"]["email"];
 
 let footerP = document.querySelector("footer");
 footerP.textContent = siteContent["footer"]["copyright"];
